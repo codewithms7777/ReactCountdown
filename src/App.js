487d32c7201079487640eb2c8a999440ccaaf993
Mahavirsinh Sodha <140199309+codewithms7777@users.noreply.github.com>
2024-12-14 23:12:04 +0530

    App.js

--- src/App.js
+++ src/App.js
@@ -6,9 +6,6 @@ function App() {
     <div className="App">
       <CountdownTimer targetDate="2024-12-31T23:59:59" />
     </div>
-  <div style="text-align: center; margin-top: 20px; font-size: 14px; color: white;">
-  &copy; 2024 All rights reserved to MS Corporation
-</div>
 
   );
 }
